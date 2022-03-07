9c4b612c68813d07eccd826acffc7ec8ff058671
VENKAT <venkat04.sekar@gmail.com>
2022-03-07 23:22:52 +0530

    Initial commit

---
+++ src/test/java/com/payment/module/service/discovery/ServiceDiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.module.service.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
